b51d319974548736258b12881cfbdf11ead03b9d
lni_T <developer.lni@gmail.com>
2015-07-02 22:52:56 +0900

    つきやすいつきにくいギアパワーはデフォルトでincludesする

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,6 +2,8 @@ class Brand < ActiveRecord::Base
   belongs_to :strong_power, class_name: "Power"
   belongs_to :weak_power, class_name: "Power"
 
+  default_scope { includes(:strong_power, :weak_power) }
+
   def description
     I18n.t("gears.common.description", name: self.name, strong: strong_power.name, weak: weak_power.name)
   end
