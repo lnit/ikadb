4962c8b1408cfde9a58ba34447263cdef5d1fb96
lni_T <developer.lni@gmail.com>
2015-08-02 19:51:06 +0900

    ギアのレコードをCSVから生成するように変更(#11)

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -15,8 +15,21 @@ class Gear < ActiveRecord::Base
   default_scope { includes(:brand, :power).order(:gear_type).order(:slot).order(:brand_id) }
 
   validates :name, uniqueness: true
+  validates :gear_type, presence: true
   validates :brand, presence: true
   validates :power, presence: true
+
+  def type_name=(name)
+    # 面倒になったので日本語ベタ書きで
+    self.gear_type = case name
+      when "アタマ" then
+        HEAD_ID
+      when "フク" then
+        BODY_ID
+      when "クツ" then
+        SHOES_ID
+    end
+  end
 
   def brand_name=(name)
     self.brand = Brand.find_by(name: name)
