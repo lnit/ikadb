57dd7c7c421583c3f39e5f7ea453b0e8a14c6d20
lni_T <developer.lni@gmail.com>
2015-08-04 02:44:05 +0900

    rails g active_admin:install(#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get "weapons" => "weapons#index"
   get "gears" => "gears#index"
 
