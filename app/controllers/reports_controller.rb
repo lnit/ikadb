974d12612b76b5bae704e659f385abd367ede271
lni_T <developer.lni@gmail.com>
2015-08-06 01:41:28 +0900

    情報提供用ページを作成(#12)

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,7 @@
+class ReportsController < ApplicationController
+  def show
+    respond_to do |format|
+      format.html
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get "weapons" => "weapons#index"
   get "gears" => "gears#index"
+  get "report" => "reports#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
