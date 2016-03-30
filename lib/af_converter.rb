93d4ef9629c40b2a9615798e4e029da7d639d08c
lni_T <developer.lni@gmail.com>
2016-03-31 03:40:52 +0900

    アルファベットをAF変換

--- lib/af_converter.rb
+++ lib/af_converter.rb
@@ -10,6 +10,29 @@ module AfConverter
       ret = ret.tr("ァ-ン","ぁ-ん")
       ret = ret.tr("ヴ","ゔ")
       ret = ret.tr("0-9", "〇一二三四五六七八九")
+      ret.gsub!(/α/, "あるふぁ")
+      ret.gsub!(/β/, "べーた")
+      ret.gsub!(/ZAP/, "ざっぷ")
+      ret.gsub!(/KOG/, "こぐ")
+      ret.gsub!(/amiibo/, "あみーぼ")
+      ret.gsub!(/bit/, "びっと")
+      ret.gsub!(/Hi/, "はい")
+      ret.gsub!(/WHITE/, "ほわいと")
+      ret.gsub!(/BLACK/, "ぶらっく")
+      ret.gsub!(/B/, "びー")
+      ret.gsub!(/C/, "しー")
+      ret.gsub!(/F/, "えふ")
+      ret.gsub!(/D/, "でぃー")
+      ret.gsub!(/G/, "えむ")
+      ret.gsub!(/H/, "えいち")
+      ret.gsub!(/K/, "けー")
+      ret.gsub!(/L/, "える")
+      ret.gsub!(/M/, "えむ")
+      ret.gsub!(/N/, "えぬ")
+      ret.gsub!(/R/, "あーる")
+      ret.gsub!(/T/, "てぃー")
+
+      ret
     end
   end
 end
